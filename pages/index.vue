17eef8f832e12c364b5f12fe0f67a3fbefde874b
Md. Shrabon Hossain <90843040+shrabon-dev@users.noreply.github.com>
2025-02-23 12:05:49 +0600

    integration section done

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <template>
     <Hero />
+    <Integretion />
 </template>
 <script setup>
      import Hero from '~/components/Hero.vue';
+     import Integretion from '~/components/Integration.vue';
 </script>
