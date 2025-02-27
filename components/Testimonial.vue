4c4f68c98ee03888bfe5b23910c171750c06e417
shrabon-dev <shrabonhossain.dev@gmail.com>
2025-02-28 01:13:36 +0600

    testimonial section responsive done

--- components/Testimonial.vue
+++ components/Testimonial.vue
@@ -16,25 +16,33 @@
       <div class="relative">
         <swiper-container ref="containerRef"
           :slides-per-view="3"
+          :breakpoints="{
+              768: {
+                slidesPerView: 3,
+              },
+              300: {
+                slidesPerView: 1,
+              },
+            }"
           speed="500"
           loop="true"
           direction="vertical"
           class="reviews_box h-[600px]"
-          @swiper="onSwiper"
+        
         >
           <!-- Group reviews into chunks of 3 -->
           <swiper-slide
             v-for="(chunk, index) in chunkedReviews"
             :key="index"
-            class="review_item"
+            class="review_item lg:mb-10 xl:mb-0 flex justify-between gap-3"
           >
-            <div class="grid grid-cols-1 sm:grid-cols-3 gap-4">
+            <div class="">
               <div
                 v-for="(review, subIndex) in chunk"
                 :key="subIndex"
-                class="bg-white rounded-2xl p-4"
+                class="bg-white inline-block md:w-[30%] m-3 rounded-2xl p-4"
               >
-                <p class="font-inter text-black text-base font-light italic">
+                <p class="font-inter text-black text-xs lg:text-base font-light italic">
                   "{{ review.quote }}"
                 </p>
                 <div class="info flex items-center gap-5 pt-3">
